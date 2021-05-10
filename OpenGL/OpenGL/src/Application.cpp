302ff38e773e7a5f2be2dda6051509fd6f4c0cf7
ain-crad <851056505@qq.com>
2021-05-10 16:20:47 +0800

    add rendering multi objects

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -55,10 +55,10 @@ int main(void) {
 
 	{
 		float positions[] = {
-			100.0f, 100.0f, 0.0f, 0.0f, // 0
-			200.0f, 100.0f, 1.0f, 0.0f, // 1
-			200.0f, 200.0f, 1.0f, 1.0f, // 2
-			100.0f, 200.0f, 0.0f, 1.0f, // 3
+			-50.0f, -50.0f, 0.0f, 0.0f, // 0
+			 50.0f, -50.0f, 1.0f, 0.0f, // 1
+			 50.0f,  50.0f, 1.0f, 1.0f, // 2
+			-50.0f,  50.0f, 0.0f, 1.0f, // 3
 		};
 
 		unsigned int indices[] = {
@@ -79,7 +79,7 @@ int main(void) {
 		IndexBuffer ib(indices, 6);
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
@@ -107,7 +107,8 @@ int main(void) {
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
 		ImGui_ImplOpenGL3_Init(glsl_version);
 
-		glm::vec3 translation(200, 200, 0);
+		glm::vec3 translationA(200, 200, 0);
+		glm::vec3 translationB(400, 400, 0);
 
 		float r = 0.0f;
 		float increment = 0.05f;
@@ -122,17 +123,24 @@ int main(void) {
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
 
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-			glm::mat4 mvp = proj * view * model;
 
-			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-			shader.SetUniformMat4f("u_MVP", mvp);
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
+			
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
 
-			va.Bind();
-			ib.Bind();
 
-			renderer.Draw(va, ib, shader);
 
 			if (r > 1.0f)
 				increment = -0.05f;
@@ -146,7 +154,8 @@ int main(void) {
 				static int counter = 0;
 
 				ImGui::Begin("Hello, world!");  // Create a window called "Hello, world!" and append into it.
-				ImGui::SliderFloat3("float", &translation.x, 0.0f, 960.0f);	// Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::SliderFloat3("floatA", &translationA.x, 0.0f, 960.0f);
+				ImGui::SliderFloat3("floatB", &translationB.x, 0.0f, 960.0f);
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 				ImGui::End();
 			}
